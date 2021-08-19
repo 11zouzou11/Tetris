e57457f5eff0558a63d9d04832f73ebdedaf674d
11zouzou11 <josephchahine@live.com>
2021-08-19 22:10:02 +0000

    added a collision function : player-arena

--- tetris.js
+++ tetris.js
@@ -10,11 +10,36 @@ const matrix = [
     [1,1,1],
     [0,1,0],
 ]
+
+
+function collision(arena, player) {
+    const m = player.matrix;
+    const o = player.position;
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+               (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
 // the draw fucntion 
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
+    drawMatrix(arena, {x:0, y:0});
     drawMatrix(player.matrix, player.position);
 }
 
@@ -30,7 +55,27 @@ function drawMatrix(matrix, offset) {
             }
         })
     });
+} 
+
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.positon.y][x + player.position.x] = value;
+            }
+        });
+    });
 }
+function playerDrop() {
+    player.position.y++;
+    if (collision(arena, player)) {
+        player.position.y--;
+        merge(arena, player);
+        player.position.y = 0;
+    }
+    dropCounter = 0;
+}
+
 
 let dropCounter = 0;
 let dropInterval = 1000; // ms is 1 sec 
@@ -45,12 +90,16 @@ function update(time = 0){
 
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        player.position.y++;
-        dropCounter = 0 ;
+        playerDrop();
     }
     draw();
     requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
+
+
+
 const player = {
     position: {x:5, y:5},
     matrix: matrix,
@@ -62,11 +111,11 @@ document.addEventListener('keydown', event => {
     } else if (event.keyCode === 39) {
         player.position.x++;
     } else if (event.keyCode === 40) {
-        player.position.y++;
-        dropCounter = 0 ;
+        playerDrop();
     }
 });
 
 
 update();
+
 
