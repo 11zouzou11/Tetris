b40b9253a7cea4498ee812b94a3db385fbec5880
11zouzou11 <josephchahine@live.com>
2021-08-19 22:39:32 +0000

    added the 7 tetrominos

--- tetris.js
+++ tetris.js
@@ -3,11 +3,6 @@ const context = canvas.getContext("2d");
 
 context.scale(20,20);
 
-const matrix = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0],
-]
 
 // collision function between the player and the walls 
 function collision(arena, player) {
@@ -33,6 +28,55 @@ function createMatrix(w, h) {
     }
     return matrix;
 }
+
+function createPiece(type)
+{
+    if (type === 'I') {
+        return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ];
+    } else if (type === 'L') {
+        return [
+            [0, 2, 0],
+            [0, 2, 0],
+            [0, 2, 2],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [3, 3, 0],
+        ];
+    } else if (type === 'O') {
+        return [
+            [4, 4],
+            [4, 4],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [5, 5, 0],
+            [0, 5, 5],
+            [0, 0, 0],
+        ];
+    } else if (type === 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === 'T') {
+        return [
+            [0, 7, 0],
+            [7, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
+
+
 // the draw fucntion 
 function draw() {
     context.fillStyle = '#000';
