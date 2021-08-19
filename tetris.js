5fa8916b4e9b5c954c2e5b28f48bc823aab0b8dd
11zouzou11 <josephchahine@live.com>
2021-08-19 11:23:03 +0000

    made the tetris piece drops every second

--- tetris.js
+++ tetris.js
@@ -3,14 +3,22 @@ const context = canvas.getContext("2d");
 
 context.scale(20,20);
 
-context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
 
 const matrix = [
     [0,0,0],
     [1,1,1],
     [0,1,0],
 ]
+
+function draw() {
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    drawMatrix(player.matrix, player.position);
+}
+
+
 function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -21,4 +29,28 @@ function drawMatrix(matrix, offset) {
         })
     });
 }
-drawMatrix(matrix, {x:5, y:5});
+let dropCounter=0;
+let dropInterval = 1000; // ms that means 1 sec 
+let lastTime = 0;
+
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    console.log(deltaTime);
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        player.position.y++;
+        dropCounter = 0 ;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+const player = {
+    position:{x:5, y:5},
+    matrix: matrix,
+}
+
+update();
+
