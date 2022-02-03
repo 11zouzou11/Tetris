1b8bdb7e4330660470842bae22b9600c11edb1ce
11zouzou11 <josephchahine@live.com>
2022-02-03 11:04:04 +0000

    added play and new game button with some style

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let board = document.querySelector('.grid-section')
+let board = document.querySelector('.board-section')
 
-for (let i= 0; i < 200; i++) {
+for (let i = 0; i < 200; i++) {
     let block = '<div class="block"></div>'
     board.innerHTML += block
 }
