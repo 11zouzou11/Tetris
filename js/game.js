f24b585b3c7074e2e6c4f59565c14a7407ca0b17
11zouzou11 <josephchahine@live.com>
2022-02-03 11:51:50 +0000

    add event listener for buttons

--- js/game.js
+++ js/game.js
@@ -8,9 +8,22 @@ btns.forEach(e => {
         switch(btn_id) {
             case 'btn-play':
                 body.classList.add('play')
+                if (body.classList.contains('pause')) {
+                    body.classList.remove('pause')
+                }
                 break
             case 'btn-theme':
                 body.classList.toggle('dark')
+                break
+            case 'btn-pause':
+                let btn_play = document.querySelector('#btn-play')
+                btn_play.innerHTML = 'resume'
+                body.classList.remove('play')
+                body.classList.add('pause')
+                break
+            case 'btn-new-game':
+                body.classList.add('play')
+                body.classList.remove('pause')
                 break
         }
     })
